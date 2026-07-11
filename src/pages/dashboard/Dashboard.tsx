77b063c236f635b37b06e8e29a13990593752bb2
kavishankha <kavishankakodithuwakku12345@gmail.com>
2026-07-11 20:01:17 +0530

    [Assesment]- add filter in to dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,15 @@
-// import {ColumnChart, Filter} from "@dashboardComponents";
+import {ColumnChart, Filter} from "@dashboardComponents";
 import { Grid, GridItem, Box } from "@chakra-ui/react"
 
 export const Dashboard = () => {
     return (
-        <Box width="100%" h="100vh" padding="4" color="white">
+        <Box width="100%" h="100vh" padding="10">
             <Grid templateColumns="repeat(3, 1fr)" h="100%" border="2px solid black">
-                <GridItem colSpan={1}>
-                    <Box bg="pink" h="100%" />
+                <GridItem colSpan={1}  padding="8">
+                    <Filter/>
                 </GridItem>
                 <GridItem colSpan={2}>
-                    <Box bg="white" h="100%" />
+                    <ColumnChart/>
                 </GridItem>
             </Grid>
         </Box>
