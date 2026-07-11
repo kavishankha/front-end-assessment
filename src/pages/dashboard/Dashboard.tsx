5c455155ee5e7c43e3f7eb85b0c1861b33f1e3d9
kavishankha <kavishankakodithuwakku12345@gmail.com>
2026-07-11 17:40:31 +0530

    [Assesment]- UI  base grid plan

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,17 @@
-import {ColumnChart, Filter, PieChart} from "@dashboardComponents";
+// import {ColumnChart, Filter} from "@dashboardComponents";
+import { Grid, GridItem, Box } from "@chakra-ui/react"
 
 export const Dashboard = () => {
-    return <div>
-        <Filter/>
-        <PieChart/>
-        <ColumnChart/>
-        <text>this is dashboard</text>
-    </div>;
+    return (
+        <Box width="100%" h="100vh" padding="4" color="white">
+            <Grid templateColumns="repeat(3, 1fr)" h="100%" border="2px solid black">
+                <GridItem colSpan={1}>
+                    <Box bg="pink" h="100%" />
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Box bg="white" h="100%" />
+                </GridItem>
+            </Grid>
+        </Box>
+    )
 };
