a95287b74af2a4f4f8d0b2999bcd4f4bb7a9e102
kavishankha <kavishankakodithuwakku12345@gmail.com>
2026-07-12 13:09:53 +0530

    [Assesment]-add redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Provider } from "@/components/ui/provider"
+import {Provider as ReduxProvider} from "react-redux"
+import {store} from "./store"
 // import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <ReduxProvider store={store}>
         <Provider>
             <App/>
         </Provider>
+        </ReduxProvider>
     </StrictMode>,
 )
